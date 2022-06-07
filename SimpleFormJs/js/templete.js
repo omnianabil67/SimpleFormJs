3c629ff23c663d353feb6f716ce11007f0008cb5
omnianabil67 <92180455+omnianabil67@users.noreply.github.com>
2022-06-07 13:57:57 +0200

    Add files via upload

---
+++ SimpleFormJs/js/templete.js
@@ -0,0 +1,43 @@
+let eye= document.querySelector(".icon");
+let checkpassword = document.querySelector(".checkpassword");
+
+
+// بعد كده هقوله ان لما اعمل حدث كليك اعملي وظيغة اني اغير من تيكست لباسبورد
+// eye.addEventListener('click',function(){
+//     checkpassword.setAttribute("type","text");
+// })
+
+// eye.addEventListener('mousemove',function(){
+//     checkpassword.setAttribute("type","text");
+// })
+
+
+// eye.addEventListener('mouseout',function(){
+//     checkpassword.setAttribute("type","text");
+// })
+
+
+
+// eye.addEventListener('mouseout',function(){
+//     let valuetype = checkpassword.getAttribute("type");
+//     if(valuetype == "text"){
+//         checkpassword.setAttribute("type","password")
+//     } else {
+//         checkpassword.setAttribute("type","text")
+//     }
+// })
+
+// let checkEmail= document.querySelector(".check-email");
+// checkEmail.addEventListener('blur',function(){
+//     let valuetype=checkEmail.value;
+//     console.log("valuetype")
+//     if(valuetype == ""){
+//                 checkEmail.classList.add("border","border-danger")
+//             } else {
+//                 checkEmail.classList.add("border","border-success")
+//             }
+//         })
+
+
+
+
